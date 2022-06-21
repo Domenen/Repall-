068cc38d755faa83f49d9dfe2a6def359714dab4
Beschastniy Sergey <Diman.9533@gmail.com>
2022-06-21 18:41:28 +0300

    modified:   homework_sql_and_py/user.py 	new file:   sochi_athletes.sqlite3

--- homework_sql_and_py/user.py
+++ homework_sql_and_py/user.py
@@ -1,6 +1,4 @@
-# import imp
 import uuid
-# import datetime
 import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -14,8 +12,9 @@ Base = declarative_base()
 class User(Base):
     """Описывает структуру таблицы user для хранения регистрационных данных пользователей
     """
-    __tablename__ = 'user'
-    id = sa.Column(sa.String(36), primory_key=True)
+    __tablename__ = "user"
+
+    id = sa.Column(sa.INTEGER, primary_key=True, autoincrement=True)
     first_name = sa.Column(sa.TEXT)
     last_name = sa.Column(sa.TEXT)
     email = sa.Column(sa.TEXT)
@@ -35,6 +34,15 @@ def connect_db():
     # возвращаем сессию
     return session()
 
+
+def valid_date(date):
+    D, M, Y = date.split(".")
+    if len(D) == 2 and len(M) == 2 and len(Y) == 4:
+        return True
+    else:
+        return False
+
+
 def request_data():
     """Запрашивает у пользователя данные и добавляет их в список users
     """
@@ -42,19 +50,19 @@ def request_data():
     first_name = input("Введе свое имя: ")
     last_name = input("Введи свою фамилию: ")
     gender = input("Введите пожалуйста ваш пол: ")
-    hieght = input("Ващ рост: ")
-    birthday = input("Когда вы родились(пожалуйста в формате 'dd.mm.yy'): ")
+    height = input("Ваш рост: ")
+    birthdate = input("Когда вы родились(пожалуйста в формате 'dd.mm.yyyy'): ")
     email = input("Введи свою почту: ")
     user_id = str(uuid.uuid4())
     # Создаем нового пользователя
     user = User(
-        id=user_id,
-        first_name=first_name,
-        last_name=last_name,
-        email=email,
-        gender=gender,
-        hieght=hieght,
-        birthday=birthday
+        id = user_id,
+        first_name = first_name,
+        last_name = last_name,
+        email = email,
+        gender = gender,
+        height = height,
+        birthdate = birthdate
     )
     # возвращаем созданного пользователя
     return user
@@ -64,30 +72,11 @@ def main():
     Осуществляет взаимодействие с пользователем, обрабатывает пользовательский ввод
     """
     session = connect_db()
-    # просим пользователя выбрать режим
-    mode = input("Выбери режим.\n1 - найти пользователя по имени\n2 - ввести данные нового пользователя\n")
-    # проверяем режим
-    if mode == "1":
-        # выбран режим поиска, запускаем его
-        name = input("Введи имя пользователя для поиска: ")
-        # вызываем функцию поиска по имени
-        users_cnt, user_ids, log = find(name, session)
-        # вызываем функцию печати на экран результатов поиска
-        print_users_list(users_cnt, user_ids, log)
-    elif mode == "2":
-        # запрашиваем данные пользоватлея
-        user = request_data()
-        # добавляем нового пользователя в сессию
-        session.add(user)
-        # обновляем время последнего визита для этого пользователя
-        # log_entry = update_timestamp(user.id, session)
-        # добавляем объект log_entry в сессию
-        # session.add(log_entry)
-        # сохраняем все изменения, накопленные в сессии
-        session.commit()
-        print("Спасибо, данные сохранены!")
-    else:
-        print("Некорректный режим:(")
+    obj = request_data()
+    session.add(obj)
+    session.commit()
+    print("Дфнные успещшно записаны")
+
 
 if __name__ == "__main__":
     main()
