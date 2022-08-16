84aacc4c7afda1779554600d254a023ac62420a0
Beschastniy Sergey <Diman.9533@gmail.com>
2022-08-16 20:52:13 +0300

    new html and css: infinity line

--- django/test_1/news/models.py
+++ django/test_1/news/models.py
@@ -5,10 +5,10 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_lengh=150)
+    title = models.CharField(max_length=150)
     content = models.TextField(blank=True)
     creared_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
     is_published = models.BooleanField(default=True) 
-    # sad
+    
