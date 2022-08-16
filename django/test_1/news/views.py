27b50ff1d247683776ca7e95a375ae4f91b4bace
Beschastniy Sergey <Diman.9533@gmail.com>
2022-08-16 16:34:14 +0300

    add 'news.test'

--- django/test_1/news/views.py
+++ django/test_1/news/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def index(request):
+    # print(request)
+    return HttpResponse("hello world")
+
+
+def test(request):
+    return HttpResponse("<h1>Testing<h1>")
