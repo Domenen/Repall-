e927f479112c97130cf27501eef1f5588c73982a
Beschastniy Sergey <Diman.9533@gmail.com>
2022-09-14 17:05:53 +0300

    work urls in news

---
+++ django/ProjectDjango/news/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    # print(dir(request))
+    return HttpResponse('Hello World')
+
+def test(request):
+    return HttpResponse('<h1>Test</h1>')
