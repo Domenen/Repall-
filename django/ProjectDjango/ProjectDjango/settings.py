e88bd29ca816a90a3f3f1fa091af852980209218
Beschastniy Sergey <Diman.9533@gmail.com>
2022-09-14 16:04:18 +0300

    modified:   .idea/misc.xml 	modified:   .idea/workspace.xml 	modified:   ProjectDjango/ProjectDjango/settings.py 	../.idea/ 	../asyncio.txt

--- django/ProjectDjango/ProjectDjango/settings.py
+++ django/ProjectDjango/ProjectDjango/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rm!5o@f@-m#61lr@l2b&gtyx4fj0m_8q=-h(1qb^-2u%y^0^1a'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
