2a38f9ecf1f13cd30002b0ebb8fc9dd57c15f308
Beschastniy Sergey <Diman.9533@gmail.com>
2022-09-20 13:41:12 +0300

    new content in db

--- django/ProjectDjango/ProjectDjango/urls.py
+++ django/ProjectDjango/ProjectDjango/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +28,4 @@ urlpatterns = [
 
 
 if settings.DEBAG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
